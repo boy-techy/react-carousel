b889f69ad4472d3b4a5c91d9bf1552344e3245ec
Yatindra Kumar Khandal <yatindra.kumar@tothenew.com>
2019-12-27 18:19:02 +0530

    key error fixes in Carousle Component

--- src/components/common/productCarousel/index.js
+++ src/components/common/productCarousel/index.js
@@ -5,8 +5,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"
 const ProductCarousel = React.memo(({ data }) => {
     return <Carousel>
         {
-            data.map(item => {
-                return <div>
+            data.map((item, index) => {
+                return <div key={index}>
                     <img src={item.url} alt={item.title}/>
                     <p className="legend">{item.title}</p>
                 </div>
